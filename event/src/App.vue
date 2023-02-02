7e0ac1acd92e4f195d72fa5a08bcd223f0b6ac22
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-02-02 22:50:02 +0900

    Feat: 이벤트 캡쳐링과 이벤트 버블링 확인 예제 추가

---
+++ event/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ChildrenComponent></ChildrenComponent>
+  </div>
+</template>
+
+<script>
+import ChildrenComponent from './components/ChildrenComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ChildrenComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ event/src/components/ChildrenComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Children</div>
+</template>
+
+<script>
+export default {
+  name: 'ChildrenComponent',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
