9b0876c4c8341b490cedac51fcea27b6e8cfa3f5
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-02-05 19:16:39 +0900

    Remove: 실습환경으로 컴포넌트 구조 변경

---
+++ event/src/components/TooltipedMenuIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-tooltip>
+    <template>
+      sdafsdf
+      <v-icon>{{ icon }}</v-icon>
+    </template>
+    아이콘
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'TooltipedMenuIcon',
+  props: ['icon'],
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ event/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-list-item>
+      <TooltipedMenuIcon icon="mdi-logout" />
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+import TooltipedMenuIcon from '@/components/TooltipedMenuIcon.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    TooltipedMenuIcon,
+  },
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
